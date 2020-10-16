287d3e01f17c1b4bd1197d7ac79d428f1bf1e642
JaneOma <janeprecioulove@gmail.com>
2020-10-16 03:27:14 -0700

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { TestBed, inject } from '@angular/core/testing';
+
+import { ProductService } from './product.service';
+
+describe('ProductService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ProductService]
+    });
+  });
+
+  it('should be created', inject([ProductService], (service: ProductService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+
 
 @NgModule({
   declarations: [
